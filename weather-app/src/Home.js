1ad829187679cd7e9b04a7bae0631801d5d667a6
Mahlatsek29 <124674856+Mahlatsek29@users.noreply.github.com>
2023-11-03 10:59:59 +0200

    update

--- weather-app/src/Home.js
+++ weather-app/src/Home.js
@@ -1,57 +1,55 @@
-import React, {useState, useEffect}from 'react'
-import './style.css'
-import axios from 'axios'
-import cloudsImage from './images/clouds.png';
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import axios from 'axios';
+import search from './images/search.jpg';
+import clouds from './images/clouds.png';
 
 function Home() {
   const [data, setData] = useState({
     celcius: 10,
     name: "London",
     humidity: 10,
-    speed:2
-  })
-  useEffect(()=>{
+    speed: 2
+  });
+
+  useEffect(() => {
     const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=462e1dc9c41358e13659fbe8c30ae5a0&units=metric";
     axios.get(apiUrl)
-    .then(res => {
-      setData({ ...data, celcius: res.data.main.temp, 
-        name: res.data.name, 
-        humidity: res.data.main.humidity,
-        speed: res.data.wind.speed})
-    })
-    .catch( err => console.log(err));
-  },[])
+      .then(res => {
+        setData({ ...data, celcius: res.data.main.temp, name: res.data.name, humidity: res.data.main.humidity, speed: res.data.wind.speed });
+      })
+      .catch(err => console.log(err));
+  }, [data]);
+
   return (
     <div className='container'>
-        <div className="weather">
-            <div className='search'>
-                <input type="text"
-                placeholder='Enter City Name'/>
-                <button><img src="/images/clouds.png" alt/></button>
-            </div>
-            <div className="winfo">
-              <img src="" alt=""/>
-              <h1>{data.celcius}°c</h1>
-              <h2>{data.name}</h2>
-              <div className='details'>
-                <div className='col'>
-                  <img src='/' alt=''/>
-                </div>
-                <p>{data.humidity}</p>
-                <p>Humidity</p>
-                </div>
-                <div className='col'>
-                <div className='col'>
-                  <img src='/' alt=''/>
-                </div>
-                <p>2 km/h</p>
-                <p>{data.wind}</p>
-                </div>
-              </div>
-            </div>
+      <div className="weather">
+        <div className='search'>
+          <input type="text" placeholder='Enter City Name' />
+          <button><img src={search} alt="Search" /></button> 
         </div>
-      
-  )
+        <div className="winfo">
+          <img src={clouds} alt="Clouds" />
+          <h1>{data.celcius}°c</h1>
+          <h2>{data.name}</h2>
+          <div className='details'>
+            <div className='col'>
+              <img src='/' alt='' />
+            </div>
+            <p>{data.humidity}</p>
+            <p>Humidity</p>
+          </div>
+          <div className='col'>
+            <div className='col'>
+              <img src='/' alt='' />
+            </div>
+            <p>2 km/h</p>
+            <p>{data.speed}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
